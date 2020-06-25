1b55e0147e49017adcea6bc9a3b738930f64a182
nico4021 <nico-bone@hotmail.com>
2020-06-24 22:23:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pymes2020-v8-bone
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/pymes2020-v8-bone)
---
+++ src/app/models/empresa.ts
@@ -0,0 +1,6 @@
+export class Empresa{
+  CantidadEmpleados: number;
+  FechaFundacion: Date;
+  IdEmpresa: number;
+  RazonSocial: string;
+}
